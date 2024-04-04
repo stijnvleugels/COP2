40646f773b0929a6a9b3b27b9e99066d1b0bb8fb
angelpranger <114067709+angelpranger@users.noreply.github.com>
2024-04-04 19:28:51 +0200

    Efficient delta_hami computation

--- isingmodel.py
+++ isingmodel.py
@@ -7,8 +7,8 @@ def init_lattice(lattice_axis_length:int, ndim:int=2) -> np.ndarray:
     size = tuple([lattice_axis_length]*ndim)
     return np.random.choice([-1, 1], size=size)
 
-def hamiltonian(lattice:np.ndarray) -> float:
-    ''' Calculate the Hamiltonian of the lattice '''
+def complete_hamiltonian(lattice:np.ndarray) -> float:
+    ''' Calculate the Hamiltonian of the entire lattice '''
     coupling_constant = 1.0  
     external_field = 0.0
     hami = 0
@@ -18,11 +18,12 @@ def hamiltonian(lattice:np.ndarray) -> float:
     for dim in range(lattice.ndim):
         nearneighbours_sumterm = lattice * np.roll(lattice, 1, axis=dim) + lattice * np.roll(lattice, -1, axis=dim)
         hami += - coupling_constant * np.sum(nearneighbours_sumterm)
-    hami +=  - external_field * np.sum(lattice)
+    hami = hami/2 # correcting for double counting of each pair of neighbours
+    hami += - external_field * np.sum(lattice)
 
     return hami
 
-def metropolis(lattice_old:np.ndarray, temperature:float, ndim:int=2) -> np.ndarray:
+def metropolis(lattice_old:np.ndarray, temperature:float, hamiltonian:float, ndim:int=2) -> np.ndarray:
     ''' Perform one step of the Metropolis algorithm.
         Flips a random spin, and accepts or rejects the flip based on the change in energy.
         Returns the updated lattice.
@@ -32,54 +33,61 @@ def metropolis(lattice_old:np.ndarray, temperature:float, ndim:int=2) -> np.ndar
     N = lattice_old.shape[0]
 
     slice_idx = [np.random.randint(0, N) for _ in range(ndim)]
-    # calculate the change in energy if we flip the spin at that point
-    hami_old = hamiltonian(lattice_old)
     lattice_new[tuple(slice_idx)] *= -1
-    hami_new = hamiltonian(lattice_new)
+    # calculate the change in energy if we flip the spin at that point
+    sum_neighbours = lattice_old[(slice_idx[0]+1) % lattice_axis_length, slice_idx[1]] \
+                    + lattice_old[(slice_idx[0]-1) % lattice_axis_length, slice_idx[1]] \
+                    + lattice_old[slice_idx[0], (slice_idx[1]+1) % lattice_axis_length] \
+                    + lattice_old[slice_idx[0], (slice_idx[1]-1) % lattice_axis_length]
+    delta_hami = - sum_neighbours * (lattice_new[tuple(slice_idx)] - lattice_old[tuple(slice_idx)])
     
-    # accept the flip if E decreases, or with probability exp(-beta * delta_E)
-    delta_hami = hami_new - hami_old
-    if delta_hami <= 0:
-        return lattice_new # accept flip
-    else:
-        if np.exp(-beta * delta_hami) < np.random.rand():
-            return lattice_old # reject flip if random number is greater than acceptance probability
-        
-    return lattice_new
+    # reject the flip only if E increases with probability 1-exp(-beta * delta_E)
+    if ((delta_hami > 0) & (np.exp(-beta * delta_hami) < np.random.rand())):
+        return lattice_old, hamiltonian # reject flip if random number between 0 and 1 is greater than acceptance probability
+    
+    hamiltonian += delta_hami
+    
+    return lattice_new, hamiltonian
 
 def equilibrate(lattice:np.ndarray, temperature:float, nsteps:int) -> np.ndarray:
     ''' Equilibrate the lattice by running the Metropolis algorithm for nsteps steps.
         Returns the magnetization of the lattice at each step.
     '''
     magnetizations = np.zeros(nsteps)
+    hamiltonian = complete_hamiltonian(lattice)
     for i in tqdm(range(nsteps)):
-        lattice = metropolis(lattice, temperature)
+        lattice, hamiltonian = metropolis(lattice, temperature, hamiltonian)
         total_magnetization = np.sum(lattice)
         magnetizations[i] = total_magnetization
 
-    return magnetizations
+    return magnetizations, lattice
 
 def main():
     ndim = 2
+    global lattice_axis_length
     lattice_axis_length = 20
-    temperature = 5
-    n_cycles = 100 # number of times we do N**2 Metropolis steps 
+    temperature = 1
+    n_cycles = 200 # number of times we do N**2 Metropolis steps 
 
     num_spins = lattice_axis_length**ndim
 
     fig, ax = plt.subplots()
-    for i in range(3): # do 3 runs because it can get stuck in a local minimum
+    for i in range(2): # do 3 runs because it can get stuck in a local minimum
         lattice = init_lattice(lattice_axis_length, ndim=2)
-        magnetizations = equilibrate(lattice, temperature, n_cycles * num_spins)
+        magnetizations, lattice_new = equilibrate(lattice, temperature, n_cycles * num_spins)
         ax.plot(np.arange(n_cycles * num_spins) / num_spins, magnetizations / num_spins, label='Magnetization')
-    ax.set_xlabel('Time [1/N^2]')
+    ax.set_xlabel('Time [1/N$^2$]')
     ax.set_ylabel('Magnetization per spin')
     plt.show()
 
     # imshow the last lattice
     fig, ax = plt.subplots()
-    ax.imshow(lattice, cmap='gray', vmin=-1, vmax=1)
+    ax.imshow(lattice_new, cmap='gray', vmin=-1, vmax=1)
     plt.show()
+
+    # temperatures = np.arange(1, 4, 0.2)
+    # for temperature in temperatures:
+        
 
     # TODO: 
     # - run for different temperatures (1 to 4 with 0.2 increments)
